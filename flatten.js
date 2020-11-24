40bbb2e9d7a0a58d6d753bb999b3e7404e92a2d3
Saad Kureshi <saq619@gmail.com>
2020-11-24 22:07:10 +0000

    Add new file

---
+++ flatten.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual([1, 2, 3], [1, 2, "3"]));
+
+const flatten = function (masterArray){
+  let flatArray = [];
+  for (let i = 0; i < masterArray.length; i++){
+    if (Array.isArray(masterArray[i])){
+      for (let j = 0; j < masterArray[i].length; j++){
+        flatArray.push(masterArray[i][j]);
+      }
+    } else {
+      flatArray.push(masterArray[i]);
+    }
+  }
+  return flatArray;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+
+console.log(flatten(["hi", [2], ["test", 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
