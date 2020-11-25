ec597c0ec9fd2007b6eff602ada47b52acf08c58
Saad Kureshi <saq619@gmail.com>
2020-11-25 03:30:43 +0000

    first commit

---
+++ middle.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const middle = function(array){
+  let middleIndex = Math.floor((array.length - 1)/ 2);
+  if (array.length === 1 || array.length === 2) {
+    return [];
+  } else if (array.length % 2 === 0){
+    
+    return [array[middleIndex], array[middleIndex + 1]];
+  } else {
+    return [array[middleIndex]];
+  }
+}
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
