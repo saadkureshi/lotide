8e7b2ae22359016b9d7161afc415220f58245de2
Saad Kureshi <saq619@gmail.com>
2020-11-26 22:13:38 +0000

    First commit

---
+++ map.js
@@ -0,0 +1,37 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const mixbag = ["1", "3", "hello", 22];
+
+const map = function(array, callback){
+  const results = [];
+  for (let item of array){
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+console.log(assertArraysEqual(map(words, word => word[0]), ["g", "c", "t", "m", "t"]));
+console.log(assertArraysEqual(map(mixbag, each => each[0]), ["1", "3", "h", undefined]));
